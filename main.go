3402482f79f753a9423a9862f6edb067af2cac98
nkondratev <nikita.kondratiev01@gmail.com>
2026-03-20 12:01:17 +0300

    delete client

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 	ws "sudoku-server/webSocket"
 
@@ -18,26 +16,12 @@ func main() {
 	r := gin.Default()
 	server := ws.NewServer()
 	m := melody.New()
-	var (
-		matchmaking = make(chan *ws.Player)
-		game        = make(chan *ws.Room)
-	)
-
-	go ws.HandleRooms(matchmaking, game)
-
-	go func() {
-		room := <-game
-		server.Append(room)
-		ws.StartGame(room)
-	}()
 
 	r.GET("/ws", func(ctx *gin.Context) {
 		m.HandleRequest(ctx.Writer, ctx.Request)
 	})
 
 	m.HandleConnect(func(s *melody.Session) {
-		player := ws.NewPlayer(s)
-		matchmaking <- player
 	})
 
 	m.HandleDisconnect(func(s *melody.Session) {
@@ -45,17 +29,6 @@ func main() {
 	})
 
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		moveDTO := &ws.MoveDTO{}
-		if err := json.Unmarshal(msg, moveDTO); err != nil {
-			log.Printf("%v", err)
-			return
-		}
-		server.Players[s].Puzzle.Grid = moveDTO.Sudoku.Grid
-		room, err := server.FindRoom(s)
-		if err != nil {
-			log.Printf("%v", err)
-			return
-		}
 	})
 
 	r.POST("/finish", func(ctx *gin.Context) {
